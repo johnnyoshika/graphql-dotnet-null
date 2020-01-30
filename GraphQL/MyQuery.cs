f9b20ee26372e351c92664e9fe9beb6b6f8d9c26
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-29 22:16:21 -0800

    Set up schema and query

---
+++ GraphQL/MyQuery.cs
@@ -0,0 +1,23 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLNull.GraphQL
+{
+    public class MyQuery : ObjectGraphType
+    {
+        public MyQuery()
+        {
+            Field<NonNullGraphType<WidgetType>>(
+                "widget",
+                resolve: context =>
+                {
+                    throw new ExecutionError("Something went wrong!");
+                }
+            );
+        }
+    }
+}
---
+++ GraphQL/WidgetType.cs
@@ -0,0 +1,21 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLNull.GraphQL
+{
+    public class Widget
+    {
+        public int Id { get; set; }
+    }
+
+    public class WidgetType : ObjectGraphType<Widget>
+    {
+        public WidgetType()
+        {
+            Field(t => t.Id);
+        }
+    }
+}
